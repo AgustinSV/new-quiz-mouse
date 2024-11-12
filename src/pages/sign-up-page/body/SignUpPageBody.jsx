ad988b36c6c25edb89f2d68c1ece97689ce4951a
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2024-11-12 00:25:17 -0500

    testing

--- src/pages/sign-up-page/body/SignUpPageBody.jsx
+++ src/pages/sign-up-page/body/SignUpPageBody.jsx
@@ -7,13 +7,16 @@ const SignUpPageBody = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+      const response = await fetch(
+        'https://new-quiz-mouse.vercel.app/api/signup',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
       if (response.ok) {
         const data = await response.json();
