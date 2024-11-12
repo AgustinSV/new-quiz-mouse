2f4fce6c6b7b7857f9e1b6c49b4220d8a9c0aa90
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2024-11-12 00:34:26 -0500

    IT IS NOW WORKING ON VERCEL DOMAIN AS WELL AS LOCALHOST!

--- api/signup.js
+++ api/signup.js
@@ -1,5 +1,4 @@
-// api/signup.js
-import connectToDatabase from './config/database.js';
+import connectToDatabase from './config/database';
 import mongoose from 'mongoose';
 
 const credentialSchema = new mongoose.Schema({
@@ -10,25 +9,19 @@ const Credential =
   mongoose.models.Credential || mongoose.model('Credential', credentialSchema);
 
 export default async function handler(req, res) {
-  console.log('Received request:', req.method, req.body);
-
   if (req.method === 'POST') {
     try {
       await connectToDatabase();
-      console.log('Database connected in signup handler');
-
       const { username, password } = req.body;
-      const newCredential = new Credential({ username, password });
 
+      const newCredential = new Credential({ username, password });
       await newCredential.save();
-      console.log('User saved:', newCredential);
 
       res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-      console.error('Error saving user:', error);
+    } catch (err) {
       res.status(500).json({ error: 'Error saving user' });
     }
   } else {
-    res.status(405).json({ error: 'Method not allowed!' });
+    req.status(405).json({ error: 'Method not allowed' });
   }
 }
