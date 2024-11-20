a763f1d237fa1f850af412a6ad18641229247a2f
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2024-11-20 18:21:05 -0500

    flashcards look a little better on main screen

--- src/pages/main-page/body/MainPageBody.jsx
+++ src/pages/main-page/body/MainPageBody.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './MainPageBody.css';
 import { useNavigate, Navigate } from 'react-router-dom';
 
+// !
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import Divider from '@mui/material/Divider';
+
+//!
 const MainPageBody = () => {
   const username = localStorage.getItem('qm_username');
   const password = localStorage.getItem('qm_password');
@@ -39,25 +47,60 @@ const MainPageBody = () => {
     <div>
       <div className="m-container">
         <div className="main-component">
-          <div className="greet-user">Welcome {username},</div>
-          <div className="header text-xl justify-center">
-            Your Flashcard Sets:
-          </div>
           <div className="flashcard-set-container">
-            {flashcardSets ? (
-              flashcardSets.map((flashcardSet, index) => (
-                <div
-                  onClick={() => sendToNewPage(flashcardSet.title)}
-                  className="flashcard-sets"
-                  key={index}>
-                  {flashcardSet.title}
-                </div>
-              ))
-            ) : (
-              <div className="loading-flashcard-sets">
-                Loading Flashcard Sets . . .
+            <div className="intro-box">
+              <div className="greet-user">Welcome {username},</div>
+              <div className="header text-xl justify-center">
+                Your Flashcard Sets:
               </div>
-            )}
+            </div>
+            <div className="flashcard-list">
+              {flashcardSets ? (
+                flashcardSets.map((flashcardSet, index) => (
+                  <div
+                    onClick={() => sendToNewPage(flashcardSet.title)}
+                    key={index}>
+                    <Box
+                      sx={{
+                        width: '100%',
+                        maxWidth: 500,
+                        bgcolor: '#DAF0F7',
+                      }}>
+                      <nav aria-label="secondary mailbox folders">
+                        <List>
+                          <ListItem disablePadding>
+                            <ListItemButton component="a">
+                              {flashcardSet.title}
+                            </ListItemButton>
+                          </ListItem>
+                        </List>
+                      </nav>
+                      <Divider />
+                    </Box>
+                  </div>
+                ))
+              ) : (
+                <div>
+                  <Box
+                    sx={{
+                      width: '100%',
+                      maxWidth: 500,
+                      bgcolor: '#DAF0F7',
+                    }}>
+                    <nav aria-label="secondary mailbox folders">
+                      <List>
+                        <ListItem disablePadding>
+                          <ListItemButton component="a">
+                            Loading flashcard sets . . .
+                          </ListItemButton>
+                        </ListItem>
+                      </List>
+                    </nav>
+                    <Divider />
+                  </Box>
+                </div>
+              )}
+            </div>
           </div>
         </div>
         <div></div>
